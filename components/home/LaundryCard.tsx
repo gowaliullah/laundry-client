60775cff66ad87f8b295f6f8bd9d6e63ebae1c47
MD Waliullah <waliullah.godev@gmail.com>
2025-10-15 13:29:02 +0600

    update laundry section

--- components/home/LaundryCard.tsx
+++ components/home/LaundryCard.tsx
@@ -12,9 +12,9 @@ const LaundryCard: FC<LaundryCardProps> = ({ name, imageSrc, rating }) => {
   const maxStars = 5;
 
   return (
-    <div className="bg-white p-4 rounded-xl shadow-md text-center flex flex-col items-center space-y-4">
+    <div className=" bg-white p-4 rounded-xl shadow-2xl text-center flex flex-col items-center space-y-4">
       {/* Circle Image */}
-      <div className="w-32 h-32 rounded-full overflow-hidden">
+      <div className="w-32 h-32 rounded-full overflow-hiden">
         <Image src={imageSrc} alt={name} width={128} height={128} className="object-cover w-full h-full" />
       </div>
 
@@ -28,8 +28,8 @@ const LaundryCard: FC<LaundryCardProps> = ({ name, imageSrc, rating }) => {
         ))}
       </div>
 
-      {/* Button (you can replace this with ShadCN Button if using) */}
-      <button className="bg-blue-500 text-white px-6 py-2 rounded-full hover:bg-blue-600 transition">
+      {/* Button */}
+      <button className="w-full bg-blue-500 text-white px-6 py-2 rounded-full hover:bg-blue-600 transition cursor-pointer">
         Visit shop
       </button>
     </div>
--- components/home/NearestLaundry.tsx
+++ components/home/NearestLaundry.tsx
@@ -1,29 +1,30 @@
+import Image from "next/image";
 import SectionTitle from "../common/SectionTitle";
 import LaundryCard from "./LaundryCard";
 
 
 const shops = [
-  { name: "Laundry Shop name", imageSrc: "/assets/laundry.jpg", rating: 3 },
-  { name: "Laundry Shop name", imageSrc: "/assets/laundry.jpg", rating: 4 },
-  { name: "Laundry Shop name", imageSrc: "/assets/laundry.jpg", rating: 3 },
-  { name: "Laundry Shop name", imageSrc: "/assets/laundry.jpg", rating: 3 },
-  { name: "Laundry Shop name", imageSrc: "/assets/laundry.jpg", rating: 4 },
-  { name: "Laundry Shop name", imageSrc: "/assets/laundry.jpg", rating: 3 },
+    { name: "Laundry Shop name", imageSrc: "https://i.ibb.co.com/MkgvQcMz/cloth.png", rating: 3 },
+    { name: "Laundry Shop name", imageSrc: "https://i.ibb.co.com/MkgvQcMz/cloth.png", rating: 4 },
+    { name: "Laundry Shop name", imageSrc: "https://i.ibb.co.com/MkgvQcMz/cloth.png", rating: 3 },
+    { name: "Laundry Shop name", imageSrc: "https://i.ibb.co.com/MkgvQcMz/cloth.png", rating: 3 },
+    { name: "Laundry Shop name", imageSrc: "https://i.ibb.co.com/MkgvQcMz/cloth.png", rating: 4 },
+    { name: "Laundry Shop name", imageSrc: "https://i.ibb.co.com/MkgvQcMz/cloth.png", rating: 3 },
 ];
 
 const NearestLaundry = () => {
-  return (
-    <>
-      <div className="bg-neutral-300">
-            <SectionTitle title="Nearest Laundry"/>
-            <div className="max-w-6xl mx-auto grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-6">
-        {shops.map((shop, index) => (
-          <LaundryCard key={index} {...shop} />
-        ))}
-      </div>
-      </div>
-    </>
-  );
+    return (
+        <>
+            <div className="bg-gray-100 py-14">
+                <SectionTitle title="Nearest Laundry" />
+                <div className="max-w-6xl mx-auto grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-6">
+                    {shops.map((shop, index) => (
+                        <LaundryCard key={index} {...shop} />
+                    ))}
+                </div>
+            </div>
+        </>
+    );
 };
 
 export default NearestLaundry;
